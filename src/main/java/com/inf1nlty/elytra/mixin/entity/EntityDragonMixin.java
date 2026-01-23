ca5abbac5a7b397ac5a1719c1698d3985dce9816
Inf1nity <1449896523@qq.com>
2026-01-23 23:12:12 +0800

    Add template drop

---
+++ src/main/java/com/inf1nlty/elytra/mixin/entity/EntityDragonMixin.java
@@ -0,0 +1,32 @@
+package com.inf1nlty.elytra.mixin.entity;
+
+import com.inf1nlty.elytra.ElytraInit;
+
+import net.minecraft.EntityDragon;
+import net.minecraft.EntityItem;
+import net.minecraft.ItemStack;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityDragon.class)
+public class EntityDragonMixin {
+
+    @Inject(method = "onDeathUpdate", at = @At("HEAD"))
+    private void elytra$dropTemplate(CallbackInfo ci) {
+        EntityDragon dragon = (EntityDragon)(Object)this;
+
+        if (dragon.deathTicks == 200 && !dragon.worldObj.isRemote) {
+            EntityItem entityItem = new EntityItem(
+                    dragon.worldObj,
+                    dragon.posX,
+                    dragon.posY,
+                    dragon.posZ,
+                    new ItemStack(ElytraInit.ELYTRA_TEMPLATE, 1)
+            );
+            dragon.worldObj.spawnEntityInWorld(entityItem);
+        }
+    }
+}
